6b8466603613ec73f6a01247caea204dbf92d53d
TanayGahlot <tanaygahlot@gmail.com>
2018-07-19 10:18:54 +0530

    Adding tokenizer(spacy) and vectorizer(Tensorflow-Hub)

--- TextCover/tokenizer.py
+++ TextCover/tokenizer.py
@@ -1,7 +1,22 @@
 """it provides tokenization facilities."""
 #oggpnson
 #hkhr
+import spacy
 
-def sentence_tokenizer(text):
+
+class Tokenizer:
+  """it tokenizes raw_text into sentences using spacy's tokenizer."""
+
+  def __init__(self):
+    """load the spacy's tokenizer."""
+    self.nlp = spacy.load('en')
+
+  def sentence_tokenizer(self, text):
     """it returns list of sentences."""
-    pass
+    # process the document using spacy's nlp kit.
+    doc = self.nlp(text)
+    # convert all the sentences into text.
+    sentences = []
+    for sentence in doc.sents:
+      sentences.append(sentence.text)
+    return sentences
--- TextCover/vectorizer.py
+++ TextCover/vectorizer.py
@@ -1,6 +1,30 @@
 """it provide utility to vectorize sentences."""
 #oggpnosn
 #hkhr
+import tensorflow_hub as hub
+import numpy as np
+import scipy
+import tensorflow as tf
+
+
+def similarity(vector_1, vector_2):
+  """it computes cosine similarity between two vectors."""
+  similarity = scipy.spatial.distance.cosine([vector_1], [vector_2])
+  return similarity
+
+def compute_adjacency_matrix(sentence_vectors):
+  """it computes distance between every two pair of sentence."""
+  # compute no of sentences.
+  no_of_sentences = sentence_vectors.shape[0]
+  # initialize adjacency_matrix with zeros.
+  adjacency_matrix = np.identity(no_of_sentences)
+  # for each sentence pair compute distance.
+  for index_1 in range(no_of_sentences-1):
+    for index_2 in range(index_1 + 1, no_of_sentences):
+      score = similarity(sentence_vectors[index_1], sentence_vectors[index_2])
+      adjacency_matrix[index_1][index_2] = score
+      adjacency_matrix[index_2][index_1] = score
+  return adjacency_matrix
 
 
 class Vectorizer:
@@ -8,8 +32,15 @@ class Vectorizer:
 
     def __init__(self):
         """it initializes tf-hub."""
-        pass
+        # creating a session.
+        self.sess = tf.Session()
+        # once the session is created load universal encoder.
+        self.embed = hub.Module("https://tfhub.dev/google/universal-sentence-encoder/2")
+        # intialize global variables.
+        self.sess.run([tf.global_variables_initializer(), tf.tables_initializer()])
 
     def vectorize_sentences(self, sentences):
         """it vectorizes sentences."""
-        pass
+        sentence_vectors = self.embed(sentences)
+        sentence_vectors = self.sess.run(sentence_vectors)
+        return sentence_vectors
